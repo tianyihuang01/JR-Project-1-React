dc4f65bd6d77d6efd5cf7d07da3076e51ca5dd7f
Tianyi Huang <thua0026@student.monash.edu>
2021-04-22 10:34:34 +1000

    install sass-loader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-			currentPage: PAGE.CONTACT,
+			currentPage: PAGE.HOME,
 		};
 
 		this.changePage = this.changePage.bind(this);
